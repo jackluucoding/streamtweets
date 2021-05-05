246ff106e3014c4aa454adf1675115c7a53fefdf
Jack Luu <53106586+jackluucoding@users.noreply.github.com>
2021-05-04 20:03:34 -0500

    Added page

--- server/index.js
+++ server/index.js
@@ -33,6 +33,7 @@ app.post('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
 })
 
+
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
 const streamURL =
   'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id'
